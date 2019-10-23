8fdfa99e15f71649de4ddba06354d1ccd3377ba7
AnotherGitAccount <poletquentin0@gmail.com>
2019-10-23 21:32:56 +0200

    Non linear laplacian, sobel and tests

--- edge_detector.py
+++ edge_detector.py
@@ -5,7 +5,6 @@
 import cv2
 import numpy as np
 
-
 def gradientOfBeucher(img, k1=5, k2=5):
     """
     TODO
@@ -25,10 +24,33 @@ def joachimLaplacianLinear(img, k1=5, k2=5):
     d = cv2.dilate(img, kernel, borderType = cv2.BORDER_CONSTANT, iterations = 1)
     return d-e
 	
-
 def canny(img, lo_thresh=40, hi_thresh=220, sobel_size=3):
     """
     TODO
     """
     return cv2.Canny(img, lo_thresh, hi_thresh, apertureSize=sobel_size,
                      L2gradient=True)
+    
+def nonLinearLaplacian(img, kernel_type=cv2.MORPH_RECT, k1=5, k2=5):
+    """
+    """
+    kernel = cv2.getStructuringElement(kernel_type, (k1, k2))
+    return cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+    
+def sobel(img, dx=1, dy=1, kernel_size=3):
+    """
+    """
+    return cv2.Sobel(img, cv2.CV_8U, dx, dy, kernel_size)
+    
+# Tests
+if __name__ == "__main__":
+    img = cv2.imread("image_database/Building.png", cv2.IMREAD_GRAYSCALE)
+    
+    cv2.imshow("Original", img)
+    cv2.imshow("Beucher", gradientOfBeucher(img))
+    cv2.imshow("Canny", canny(img))
+    cv2.imshow("NL_Lap", nonLinearLaplacian(img))
+    cv2.imshow("Sobel", sobel(img))
+
+    cv2.waitKey(0);
+    cv2.destroyAllWindows()
