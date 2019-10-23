ee1a8b4a482df211e7aa9f5dc3e39210a8c37ca2
Joachim <joachim.r96@hotmail.be>
2019-10-23 16:20:52 +0200

    Instruction v1.3 + Start of Variant of Hough Transform

--- edge_detector.py
+++ edge_detector.py
@@ -17,6 +17,15 @@ def gradientOfBeucher(img, k1=5, k2=5):
     #return d-e # This is not the Beucher Gradient but the non-linear Laplacian
     return cv2.bitwise_not(d+e) # ref : https://stackoverflow.com/questions/19580102/inverting-image-in-python-with-opencv
 
+def joachimLaplacianLinear(img, k1=5, k2=5):
+	# TODO remove this function after Quentin has done it.
+    kernel = np.ones((k1, k2), np.uint8)
+    
+    e = cv2.erode(img, kernel, borderType = cv2.BORDER_CONSTANT, iterations = 1) 
+    d = cv2.dilate(img, kernel, borderType = cv2.BORDER_CONSTANT, iterations = 1)
+    return d-e
+	
+
 def canny(img, lo_thresh=40, hi_thresh=220, sobel_size=3):
     """
     TODO
