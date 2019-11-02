d89375a432d94afb71e6f8e9e610c855589bc913
AnotherGitAccount <poletquentin0@gmail.com>
2019-11-02 02:20:57 +0100

    soccer segment detection

--- soccer.py
+++ soccer.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import edge_detector as ed
+import segment_detector as sd
 
 def go(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -9,20 +10,24 @@ def go(img):
     
     mask = cv2.inRange(hsv, low, upp)
     img_mask = cv2.bitwise_and(img, img, mask=mask)
-    img_grey = img_mask[:, :, 2]
-    _, img_thresh = cv2.threshold(img_grey, 40, 255, cv2.THRESH_BINARY)
 
-    return img_thresh
+    return img_mask[:, :, 2]
     
 if __name__ == "__main__":
     img = cv2.imread("image_database/soccer.png")
     dsp = cv2.pyrDown(img)
     gau = cv2.GaussianBlur(img, (3, 3), 2)
-    can = ed.canny_vanilla(img)
     
-    cv2.imshow("none", go(img))
+    can = ed.canny_vanilla(img)
+    non = ed.canny_vanilla(go(img))
+    
+    _, res, _ = sd.hough(non, 1, np.pi/180.0, thresh=50, minLineLen=5, maxLineGap=0, 
+                         fuse=True, dTheta=2.0/360.0*np.pi*2.0, dRho=2)
+    
+    cv2.imshow("none", non)
     cv2.imshow("dsp", go(dsp))
     cv2.imshow("gau", go(gau))
     cv2.imshow("canny", can)
+    cv2.imshow("res", res)
     cv2.waitKey()
     cv2.destroyAllWindows()
